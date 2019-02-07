0f3f688d77991df5172024d9144d5f6ca80a3778
Michael Brown <michael.brown@babylonhealth.com>
2019-02-07 21:46:02 +0000

    Initial commit.

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.1'
+
+target 'SnapshotBug' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SnapshotBug
+
+  target 'SnapshotBugTests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'SnapshotTesting', '~> 1.1'
+    pod 'ReactiveCocoa', '~> 8.0'
+  end
+
+end
--- SnapshotBugTests/SnapshotBugTests.swift
+++ SnapshotBugTests/SnapshotBugTests.swift
@@ -1,34 +1,141 @@
-//
-//  SnapshotBugTests.swift
-//  SnapshotBugTests
-//
-//  Created by Michael Brown on 07/02/2019.
-//  Copyright © 2019 Michael. All rights reserved.
-//
-
 import XCTest
 @testable import SnapshotBug
+import ReactiveCocoa
+import ReactiveSwift
+import SnapshotTesting
 
 class SnapshotBugTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testViewController() {
+        Device.all.forEach { device in
+//            autoreleasepool {
+                let viewController = UIViewController()
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+                let exp = expectation(description: "\(#file):\(#line)")
+                var window: UIWindow! = nil
+                let navigation = UINavigationController(rootViewController: viewController)
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+                viewController.reactive.signal(for: #selector(viewController.viewDidAppear(_:)))
+                    .observe(on: QueueScheduler.main)
+                    .take(duringLifetimeOf: viewController)
+                    .observeValues { _ in
+                        exp.fulfill()
+                }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+                DispatchQueue.main.async {
+                    window = self.makeWindow(for: navigation, device: device)
+                }
+
+                waitForExpectations(timeout: 5, handler: nil)
+                verify(view: window, for: device)
+//            }
         }
     }
 
+    func verify(
+        view: UIView,
+        for device: Device,
+        file: StaticString = #file,
+        function: String = #function,
+        line: UInt = #line,
+        tolerance: CGFloat = 0.01
+    ) {
+        assertSnapshot(
+            matching: view,
+            as: .image(precision: 1.0 - Float(tolerance)),
+            named: device.indentifier,
+            file: file,
+            testName: function,
+            line: line
+        )
+    }
+
+    func makeWindow(for viewController: UIViewController, device: Device) -> UIWindow {
+        let window = HostWindow(screen: device)
+        UIView.setAnimationsEnabled(false)
+        window.tintColor = .green
+        window.rootViewController = viewController
+        window.isHidden = false
+
+        return window
+    }
+
+}
+
+public enum Device {
+    case iPhone6
+    case iPhone6Plus
+    case iPhoneX
+
+    public static var all: [Device] {
+        return [iPhone6, iPhone6Plus, iPhoneX]
+    }
+
+    public var size: CGSize {
+        switch self {
+        case .iPhone6:
+            return CGSize(width: 375, height: 667)
+        case .iPhone6Plus:
+            return CGSize(width: 414, height: 736)
+        case .iPhoneX:
+            return CGSize(width: 375, height: 812)
+        }
+    }
+
+    public var indentifier: String {
+        switch self {
+        case .iPhone6:
+            return "iPhone6"
+        case .iPhone6Plus:
+            return "iPhone6Plus"
+        case .iPhoneX:
+            return "iPhoneX"
+        }
+    }
+
+    public var traits: UITraitCollection {
+        switch self {
+        case .iPhone6:
+            return .init(traitsFrom: [
+                .init(displayScale: 2),
+                .init(horizontalSizeClass: .compact),
+                .init(verticalSizeClass: .regular),
+                .init(userInterfaceIdiom: .phone)
+                ]
+            )
+        case .iPhone6Plus:
+            return .init(traitsFrom: [
+                .init(displayScale: 3),
+                .init(horizontalSizeClass: .compact),
+                .init(verticalSizeClass: .regular),
+                .init(userInterfaceIdiom: .phone)
+                ]
+            )
+        case .iPhoneX:
+            return .init(traitsFrom: [
+                .init(displayScale: 3),
+                .init(horizontalSizeClass: .compact),
+                .init(verticalSizeClass: .regular),
+                .init(userInterfaceIdiom: .phone)
+                ]
+            )
+        }
+    }
+}
+
+final class HostWindow: UIWindow {
+    private let testScreen: Device
+
+    override var traitCollection: UITraitCollection {
+        return testScreen.traits
+    }
+
+    init(screen: Device) {
+        self.testScreen = screen
+        super.init(frame: .init(origin: .zero, size: screen.size))
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
